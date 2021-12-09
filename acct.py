23881570c3fd4514511d69b498a9ec6884464c9c
tbb33 <91215510+tbb33@users.noreply.github.com>
2021-12-09 15:12:41 -0600

    Commit & Withdraw Method
    Withdraw method takes away specified amount from balance.
    Commit method updates the balance in .txt file.
    Needed to make an instance var for the file path for balance.txt so can be used anywhere in script.

--- acct.py
+++ acct.py
@@ -1,9 +1,20 @@
 class Account:
 
     def __init__(self, filepath):
+        self.newfilepath=filepath #instance var to be use anywhere in script
         with open(filepath, 'r') as file:
             self.balance=int(file.read()) #saves value of txt file inside instance var
+
+    def withdraw(self, amount):
+        self.balance=self.balance-amount
+
+    def commit(self):
+        with open(self.newfilepath, 'w') as file:
+            file.write(str(self.balance))
 
 account = Account("balance.txt") #may need to pass directory here for txt file
 print(account) #output main - occur when execute script (differs when import)
 print(account.balance) #can access attributes of obj instance using dot notation
+account.withdraw(100)
+print(account.balance)
+account.commit() #implicitely pass obj instance
